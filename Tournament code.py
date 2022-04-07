9f63294a1e024b137c5418c711aaba0cf4c03ef1
Will Stearns <Wstearns@dev-10.com>
2022-04-07 14:06:53 -0500

    Conflicting file names code

--- Tournament code.py
+++ Tournament code.py
@@ -1,4 +1,6 @@
+import os.path
 
+# first interaction page with user
 def welcome_page():
     num_participants=""
     print("Welcome to Tournaments R Us")
@@ -11,11 +13,34 @@ def welcome_page():
     print(f'\nThere are {num_participants} participant slots ready for sign-ups.')
     return num_participants
 
+# constructing the blank sign-up sheet (participant_spots)
 participant_spots={}
 number_participants=welcome_page()
 for i in range(int(number_participants)):
     participant_spots[i+1]=None
 
-tournament_name=input("What is your tournament name?  ")
+# check to see if this is the same tournamet or a different one, loop needs to run the first time as no tournament has been pre-specified
+name_conflict=True
+tournament_name=""
+while name_conflict:
+    tournament_name=input("What is your tournament name?  ")
+    if os.path.exists(f'{tournament_name}.csv'):
+        edit_file_checked = False
+        while not edit_file_checked:
+            tournament_updates=input('A tournament of that name exists, are these edits to the existing tournament [y/n]: ')
+            if tournament_updates!='y' and tournament_updates!='n':
+                print("That wasn't one of the options, please resond only 'y' or 'n'")
+            elif tournament_updates == 'n':
+                print("Please try a different tournament name.\n")
+                edit_file_checked = True
+            else:
+                print("We shall edit this tournament's file.\n")
+                edit_file_checked = True
+                name_conflict = False
+            print('answered')
+        print('out of edit')
+    else:
+        name_conflict=False
+
 file=open(f'{tournament_name}.csv','w')
 file.close()
